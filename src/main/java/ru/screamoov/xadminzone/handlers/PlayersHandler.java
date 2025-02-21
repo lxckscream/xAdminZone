aae24e8a6aeabca32db74524bc10fa79bb152403
screamoov <mityafotografer@gmail.com>
2025-02-21 20:45:24 +0300

    1

--- src/main/java/ru/screamoov/xadminzone/handlers/PlayersHandler.java
+++ src/main/java/ru/screamoov/xadminzone/handlers/PlayersHandler.java
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import ru.screamoov.xadminzone.XAdminZone;
+import ru.screamoov.xadminzone.customplayer.XPlayer;
 
 public class PlayersHandler implements Listener {
     public XAdminZone xAdminZone;
@@ -14,6 +15,10 @@ public class PlayersHandler implements Listener {
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
-
+        XPlayer xPlayer = xAdminZone.adminZoneManager.getPlayer(event.getPlayer());
+        if (xPlayer!=null) {
+            event.getPlayer().teleport(xPlayer.oldLocation);
+            xAdminZone.adminZoneManager.players.remove(xPlayer);
+        }
     }
 }
